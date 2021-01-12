dafdce492c8f6b0aec3802b25525e6dee6379466
Neeraj Agnihotri <agnihotrineeraj00@gmail.com>
2021-01-12 11:05:51 +0530

    Add project files.

---
+++ PTC.ViewModelLayer/ProductViewModel.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PTC.DataAccessLayer.EDM;
+using System.Collections.Specialized;
+using System.Data.Entity;
+namespace PTC.ViewModelLayer
+{
+  public  class ProductViewModel
+    {
+
+        public List<Product> dataCollection { get; set; }
+        public string Message { get; set; }
+        public Product productData { get; set; }
+        public int productId{ get; set; }
+        public string Event { get; set; }
+
+        public void Init()
+        {
+            dataCollection = new List<Product>();
+            Message = string.Empty;
+
+        }
+
+        public ProductViewModel() :base()
+        {
+            Init();
+
+        }
+        protected void addProduct()
+        {
+            try
+            {
+                using (var db  = new mvvmpattrenEntities())
+                {
+                    db.Products.Add(productData);
+                    db.SaveChanges();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Publish(ex, "Something Went Wrong while Adding Product data");
+            }
+           
+        }
+
+        public void Publish(Exception ex, string message)
+        {
+            Publish(ex, message, null);
+        }
+        public void Publish(Exception ex, string message,
+                            NameValueCollection nvc)
+        {
+            // Update view model properties
+            Message = message;
+            // TODO: Publish exception here	
+        }
+
+        protected void BuildCollection()
+        {
+            try
+            {
+                mvvmpattrenEntities dc = new mvvmpattrenEntities();
+                dataCollection = dc.Products.ToList();
+            }
+            catch(Exception ex)
+            {
+                Publish(ex, "Error While Loading Product");
+            }
+
+        }
+        protected void  EditProduct()
+        {
+            try
+            {
+                using(var db = new mvvmpattrenEntities())
+                {
+                    productData = db.Products.Find(productId);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Publish(ex, "Error While Finding Product");
+            }
+
+        }
+        protected void UpdateProduct()
+        {
+            try
+            {
+                using(var db  = new mvvmpattrenEntities())
+                {
+                    db.Entry(productData).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+
+            }
+            catch(Exception ex)
+            {
+
+                Publish(ex, "Error While Updating Product");
+            }
+
+        }
+
+       public void HandleRequest(string eventname)
+        {
+            switch (eventname)
+            {
+                case "Get":
+                    BuildCollection();
+                    break;
+                case "Add":
+                    addProduct();
+                    break;
+                case "Edit":
+                    EditProduct();
+                    break;
+                case "Update":
+                    UpdateProduct();
+                    break;
+                     
+
+                default:
+                    break;
+                    
+
+            }
+
+
+           
+
+        }
+        public void HandlePostRequest()
+        {
+            addProduct();
+        }
+       
+    }
+}
---
+++ PTC/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PTC.ViewModelLayer;
+namespace PTC.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductViewModel pvm = new ProductViewModel();
+        // GET: Product
+        public ActionResult Index()
+        {
+           
+            pvm.HandleRequest("Get");
+            return View(pvm);
+        }
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct(ProductViewModel product)
+        {
+            product.HandleRequest("Add");
+            return View();
+        }
+        public ActionResult EditProduct(int id)
+        {
+            pvm.productId = id;
+            pvm.HandleRequest("Edit");
+            return View(pvm);
+        }
+        [HttpPost]
+        public ActionResult EditProduct(ProductViewModel obj)
+        {
+           obj.HandleRequest("Update");
+            return RedirectToAction("Index");
+        }
+
+    }
+}
